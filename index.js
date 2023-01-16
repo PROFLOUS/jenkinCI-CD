646de7f83c5766a6b24dade268b66f15e81b46f9
tandang <cuctrau159@gmail.com>
2023-01-16 23:27:12 +0700

    te2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3004;
 
-app.get('/', (req, res) => res.send('Hello World AE!!'));
+app.get('/', (req, res) => res.send('Qon AE!!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
